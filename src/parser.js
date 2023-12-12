8263e312e6ea35c84e5cab91385650955d08c1b6
Kirill <faeeeee@gmail.com>
2023-12-12 11:42:15 +0500

    change logic, make it pure

--- src/parser.js
+++ src/parser.js
@@ -1,21 +1,14 @@
-const parseRss = (link) => {
-  const parsedRss = fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(link)}`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Network response was not ok.');
-    })
-    .then((data) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(data.contents, 'application/xml');
-      const rss = doc.querySelector('rss');
-      if (!rss) {
-        throw new Error('noRss');
-      }
+const parseRss = (request) => {
+  const parsedRss = request.then((response) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(response.data.contents, 'application/xml');
+    const rss = doc.querySelector('rss');
+    if (!rss) {
+      throw new Error('noRss');
+    }
 
-      return rss;
-    });
+    return rss;
+  });
   return parsedRss;
 };
 
