d0ef4da8132502f70ba00f3b80ddb4d78bd4633a
Kirill <faeeeee@gmail.com>
2023-12-19 23:41:18 +0500

    add new cases, move code from cases to own functions on module level

--- src/view.js
+++ src/view.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 const createCardBody = (cardTitle) => {
   const container = document.createElement('div');
   container.classList.add('card', 'border-0');
@@ -23,161 +25,196 @@ const addAttributesToElement = (element, arrOfAttributes) => {
   });
 };
 
+const feedsRender = (currentState, i18nInstance, feedsBlock) => {
+  const [currentFeed] = currentState.feeds.filter((feed) => feed.id === currentState.lastFeedId);
+
+  if (!feedsBlock.hasChildNodes()) {
+    const cardBlock = createCardBody(i18nInstance.t('feeds'));
+    feedsBlock.append(cardBlock);
+  }
+
+  const listGroup = feedsBlock.querySelector('.list-group');
+
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+  const listTitle = document.createElement('h3');
+  listTitle.classList.add('h6', 'm-0');
+  listTitle.textContent = currentFeed.title;
+
+  const listDescription = document.createElement('p');
+  listDescription.classList.add('m-0', 'small', 'text-black-50');
+  listDescription.textContent = currentFeed.description;
+
+  listItem.append(listTitle, listDescription);
+  listGroup.prepend(listItem);
+};
+
+const postsRender = (currentState, i18nInstance, postsBlock) => {
+  const postList = currentState.posts;
+
+  if (!postsBlock.hasChildNodes()) {
+    const cardBlock = createCardBody(i18nInstance.t('posts'));
+    postsBlock.append(cardBlock);
+  }
+
+  const listGroup = postsBlock.querySelector('.list-group');
+
+  const listItems = postList.map((post) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+
+    const link = document.createElement('a');
+    const linkAttributes = [
+      ['href', `${post.link}`],
+      ['target', '_blank'],
+      ['rel', 'noopener noreferrer'],
+    ];
+    addAttributesToElement(link, linkAttributes);
+
+    link.classList.add('fw-bold');
+    link.textContent = `${post.title}`;
+
+    const button = document.createElement('button');
+    const buttonAttributes = [
+      ['type', 'button'],
+      ['data-bs-toggle', 'modal'],
+      ['data-bs-target', '#modal'],
+    ];
+    addAttributesToElement(button, buttonAttributes);
+
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.textContent = i18nInstance.t('watchButton');
+
+    listItem.append(link, button);
+
+    return listItem;
+  });
+  listGroup.replaceChildren(...listItems.reverse());
+};
+
+const feedBackRender = (currentState, i18nInstance, feedback, input, form, sendingButton) => {
+  switch (currentState.form.process.state) {
+    case 'sending': {
+      sendingButton.setAttribute('disabled', '');
+      feedback.textContent = 'RSS загружается';
+      feedback.classList.replace('text-danger', 'rss-uploading');
+      feedback.classList.replace('text-success', 'rss-uploading');
+      break;
+    }
+    case 'sent': {
+      sendingButton.removeAttribute('disabled');
+      feedback.classList.replace('rss-uploading', 'text-danger');
+      break;
+    }
+    case 'filling':
+    case 'uploaded': {
+      feedback.classList.replace('text-danger', 'text-success');
+      feedback.textContent = i18nInstance.t('uploaded');
+      form.reset();
+      input.focus();
+      break;
+    }
+    case 'error': {
+      sendingButton.removeAttribute('disabled');
+      feedback.textContent = `${i18nInstance.t(`errors.${currentState.form.process.error}`)}`;
+      feedback.classList.replace('rss-uploading', 'text-danger');
+      feedback.classList.replace('text-success', 'text-danger');
+      break;
+    }
+    default: {
+      throw new Error(`Uknown error: ${currentState.form.process.state}`);
+    }
+  }
+};
+
+const errorRender = (currentState, i18nInstance, feedback, input) => {
+  switch (currentState.form.process.error) {
+    case 'invalidLink': {
+      feedback.textContent = i18nInstance.t('errors.invalidLink');
+      input.classList.add('is-invalid');
+      break;
+    }
+    case 'alreadyAdded': {
+      feedback.textContent = i18nInstance.t('errors.alreadyAdded');
+      input.classList.add('is-invalid');
+      break;
+    }
+    case 'network': {
+      feedback.textContent = i18nInstance.t('errors.network');
+      input.classList.add('is-invalid');
+      break;
+    }
+    case 'noRss': {
+      feedback.textContent = i18nInstance.t('errors.noRss');
+      input.classList.add('is-invalid');
+      break;
+    }
+    case null: {
+      input.classList.remove('is-invalid');
+      break;
+    }
+    default: {
+      throw new Error(`Unknown error: ${currentState.form.process.error}`);
+    }
+  }
+};
+
+const renderModalWindow = (currentState, modalFullArticleButton, modalTitle, modalBody) => {
+  const { description, link, title } = currentState.modalWindow.post;
+
+  modalFullArticleButton.setAttribute('href', `${link}`);
+
+  modalTitle.textContent = title;
+  modalBody.textContent = description;
+};
+
+const renderWatchedPosts = (currentState, postsBlock) => {
+  postsBlock.querySelectorAll('a').forEach((post) => {
+    const postLink = post.getAttribute('href');
+    if (currentState.watchedPostLinks.includes(postLink)) {
+      post.classList.remove('fw-bold');
+      post.classList.add('fw-normal', 'link-secondary');
+    }
+  });
+};
+
 export default (state, i18nInstance, elements) => (path) => {
   const {
     feedback, input,
     form, sendingButton,
     feedsBlock, postsBlock,
+    modalTitle, modalBody,
+    modalFullArticleButton,
   } = elements;
-
-  const feedsRender = (currentState) => {
-    const [currentFeed] = currentState.feeds.filter((feed) => feed.id === currentState.lastFeedId);
-
-    if (!feedsBlock.hasChildNodes()) {
-      const cardBlock = createCardBody(i18nInstance.t('feeds'));
-      feedsBlock.append(cardBlock);
-    }
-
-    const listGroup = feedsBlock.querySelector('.list-group');
-
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-group-item', 'border-0', 'border-end-0');
-
-    const listTitle = document.createElement('h3');
-    listTitle.classList.add('h6', 'm-0');
-    listTitle.textContent = currentFeed.title;
-
-    const listDescription = document.createElement('p');
-    listDescription.classList.add('m-0', 'small', 'text-black-50');
-    listDescription.textContent = currentFeed.description;
-
-    listItem.append(listTitle, listDescription);
-    listGroup.prepend(listItem);
-  };
-
-  const postsRender = (currentState) => {
-    const postList = currentState.posts;
-
-    if (!postsBlock.hasChildNodes()) {
-      const cardBlock = createCardBody(i18nInstance.t('posts'));
-      postsBlock.append(cardBlock);
-    }
-
-    const listGroup = postsBlock.querySelector('.list-group');
-
-    const listItems = postList.map((post) => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-
-      const link = document.createElement('a');
-      const linkAttributes = [
-        ['href', `${post.link}`],
-        ['target', '_blank'],
-        ['rel', 'noopener noreferrer'],
-      ];
-      addAttributesToElement(link, linkAttributes);
-
-      link.classList.add('fw-bold');
-      link.textContent = `${post.title}`;
-
-      const button = document.createElement('button');
-      const buttonAttributes = [
-        ['type', 'button'],
-        ['data-bs-toggle', 'modal'],
-        ['data-bs-target', '#modal'],
-      ];
-      addAttributesToElement(button, buttonAttributes);
-
-      button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-      button.textContent = i18nInstance.t('watchButton');
-
-      listItem.append(link, button);
-
-      return listItem;
-    });
-    listGroup.replaceChildren(...listItems.reverse());
-  };
 
   switch (path) {
     case 'form.process.state': {
-      switch (state.form.process.state) {
-        case 'sending': {
-          sendingButton.setAttribute('disabled', '');
-          feedback.textContent = 'RSS загружается';
-          feedback.classList.replace('text-danger', 'rss-uploading');
-          feedback.classList.replace('text-success', 'rss-uploading');
-          break;
-        }
-        case 'sent': {
-          sendingButton.removeAttribute('disabled');
-          feedback.classList.replace('rss-uploading', 'text-danger');
-          break;
-        }
-        case 'filling':
-        case 'uploaded': {
-          feedback.classList.replace('text-danger', 'text-success');
-          feedback.textContent = i18nInstance.t('uploaded');
-          form.reset();
-          input.focus();
-          break;
-        }
-        case 'error': {
-          sendingButton.removeAttribute('disabled');
-          feedback.textContent = `${i18nInstance.t(`errors.${state.form.process.error}`)}`;
-          feedback.classList.replace('rss-uploading', 'text-danger');
-          feedback.classList.replace('text-success', 'text-danger');
-          break;
-        }
-        default: {
-          throw new Error(`Uknown error: ${state.form.process.state}`);
-        }
-      }
+      feedBackRender(state, i18nInstance, feedback, input, form, sendingButton);
       break;
     }
     case 'form.process.error': {
-      switch (state.form.process.error) {
-        case 'invalidLink': {
-          feedback.textContent = i18nInstance.t('errors.invalidLink');
-          input.classList.add('is-invalid');
-          break;
-        }
-        case 'alreadyAdded': {
-          feedback.textContent = i18nInstance.t('errors.alreadyAdded');
-          input.classList.add('is-invalid');
-          break;
-        }
-        case 'network': {
-          feedback.textContent = i18nInstance.t('errors.network');
-          input.classList.add('is-invalid');
-          break;
-        }
-        case 'noRss': {
-          feedback.textContent = i18nInstance.t('errors.noRss');
-          input.classList.add('is-invalid');
-          break;
-        }
-        case null: {
-          input.classList.remove('is-invalid');
-          break;
-        }
-        default: {
-          throw new Error(`Unknown error: ${state.form.process.error}`);
-        }
-      }
+      errorRender(state, i18nInstance, feedback, input);
       break;
     }
     case 'feeds': {
-      feedsRender(state);
-
+      feedsRender(state, i18nInstance, feedsBlock);
       break;
     }
     case 'posts': {
-      postsRender(state);
-
+      postsRender(state, i18nInstance, postsBlock);
       break;
     }
     case 'lastFeedId': {
       console.log(`Last feed id has been updated, current id is ${state.lastFeedId}`);
+      break;
+    }
+    case 'modalWindow': {
+      renderModalWindow(state, modalFullArticleButton, modalTitle, modalBody);
+      break;
+    }
+    case 'watchedPostLinks': {
+      renderWatchedPosts(state, postsBlock);
       break;
     }
     default: {
